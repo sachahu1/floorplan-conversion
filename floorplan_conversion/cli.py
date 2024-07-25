50142160c7cc97e4325e28c1bf25b2edf3007d6f
Sacha <sachahu@hotmail.fr>
2024-07-25 21:41:51 +0100

    remove unused line

--- floorplan_conversion/cli.py
+++ floorplan_conversion/cli.py
@@ -7,7 +7,6 @@ def run():
 
 
 @run.command()
-# @click.argument('API_KEY', required=True)
 @click.option("--api-key", help="Your Roboflow API Key", required=True)
 def download_data(api_key: str):
   from roboflow import Roboflow
